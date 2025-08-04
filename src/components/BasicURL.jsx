bf3b8165f8baeff2df4f53f5a80a5ba9f84c502e
mzain030 <113288311+mzain030@users.noreply.github.com>
2025-08-04 17:45:54 +0500

    install react-icons library and createa a file basicURL

---
+++ src/components/BasicURL.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CgAirplane } from "react-icons/cg";
+const UserReactIcons = () => {
+  return (
+    <div>
+        <div>
+<CgAirplane style={{width:"700px",height:"300px"}}/>
+</div>
+    </div>
+  )
+}
+
+export default UserReactIcons
