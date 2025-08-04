6790f259f2d2908846e05fc28cf20cf977d2907a
mzain030 <113288311+mzain030@users.noreply.github.com>
2025-08-04 16:57:03 +0500

    react router dom

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import PropsRecived from './PropsRecived'
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
-  const [color,setColor]=useState('')
+  const [color,setColor]=useState('');
+  const history=useNavigate();
+  const moveNextPage=()=>{
+    return(
+history('/moveNextPage')
+    )
+  }
   return (
     <div>
       <h1>My current state is {color}</h1>
       <button  onClick={()=>setColor("green")}>State Update Green</button>
       <button  onClick={()=>setColor("Yellow")}>Yellow</button><hr></hr>
         Search:<input ></input>
-        <button>Click me</button>
+        <button onClick={moveNextPage}>Click me</button>
 
 <PropsRecived color={color}/>
     </div>
