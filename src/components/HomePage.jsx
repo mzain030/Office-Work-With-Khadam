b74eaabfc82c9c373f5a06d3a71eff0d06ef7ce5
mzain030 <113288311+mzain030@users.noreply.github.com>
2025-08-04 16:02:40 +0500

    mange state

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropsRecived from './PropsRecived'
 
 const HomePage = () => {
+  const [color,setColor]=useState('')
   return (
     <div>
-        Search:<input></input>
+      <h1>My current state is {color}</h1>
+      <button  onClick={()=>setColor("green")}>State Update Green</button>
+      <button  onClick={()=>setColor("Yellow")}>Yellow</button><hr></hr>
+        Search:<input ></input>
+        <button>Click me</button>
+
+<PropsRecived color={color}/>
     </div>
   )
 }
---
+++ src/components/PropsRecived.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PropsRecived = (props) => {
+  return (
+    <div>
+        <h1>Props Recived</h1>
+        <h1>{props.color}</h1>
+    </div>
+  )
+}
+
+export default PropsRecived
